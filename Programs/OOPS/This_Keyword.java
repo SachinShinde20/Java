034ff123fc2b101a2768c98ba4e650564e88fcc0
SachinShinde20 <shindesachinw@gmail.com>
2025-01-04 15:00:05 +0530

    This Concept is added

---
+++ Programs/OOPS/This_Keyword.java
@@ -0,0 +1,39 @@
+class Student {
+    private int id;
+    private String name;
+
+    /*
+     * The JVM automatically passes the reference of the calling object to a
+     * non-static method when it is invoked.
+     * This is how the method get acess to this keyword
+     */
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /*
+     * We can call another methods, variables of same class, use using this
+     * keyword, We also can pass object by passing this to a method
+     */
+    public void getCall() {
+        System.out.println("Inside getCall...");
+        this.show();
+    }
+
+    public void show() {
+        System.out.println("id: " + id);
+        System.out.println("Name: " + name + "\n");
+    }
+}
+
+public class This_Keyword {
+    public static void main(String[] args) {
+
+        Student s1 = new Student(1, "Raj");
+        Student s2 = new Student(2, "Roy");
+
+        s1.getCall();
+        s2.getCall();
+    }
+}
