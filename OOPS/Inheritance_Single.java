307c745f76c14c000ce979fb2f1a6ee129c69831
SachinShinde20 <shindesachinw@gmail.com>
2025-01-29 20:37:16 +0530

    Changes

--- OOPS/Inheritance_Single.java
+++ OOPS/Inheritance_Single.java
@@ -5,17 +5,22 @@ class Employee {
     Employee(int id, String name) {
         this.id = id;
         this.name = name;
-        show();
+    }
+}
+
+class Company extends Employee {
+    Company(int id, String name) {
+        super(id, name);
     }
 
-    public void show() {
-        System.out.println("id: " + id + " \tName: " + name);
+    public void getEmployeeInfo() {
+        System.out.println("Id: " + id + ",\tName: " + name);
     }
 }
 
 public class Inheritance_Single {
     public static void main(String[] args) {
-        new Employee(1, "Virat");
-        new Employee(2, "Rohit");
+        Company company = new Company(1, "Viraj");
+        company.getEmployeeInfo();
     }
 }
